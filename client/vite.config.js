88e685b926695b31995369fc49539afde7f844d7
FATEH BEDDIAF <62912697+madjed-hue@users.noreply.github.com>
2022-12-31 21:03:39 +0100

    fix vite.config 2

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
   },
 });
